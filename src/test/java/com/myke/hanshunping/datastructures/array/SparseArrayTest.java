98d935a95d05d213754a6c2fc12c178329dd32e9
zhangjianbin1024 <289792436@qq.com>
2021-05-22 20:55:43 +0800

    add log

--- src/test/java/com/myke/hanshunping/datastructures/array/SparseArrayTest.java
+++ src/test/java/com/myke/hanshunping/datastructures/array/SparseArrayTest.java
@@ -1,9 +1,11 @@
 package com.myke.hanshunping.datastructures.array;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+@Slf4j
 public class SparseArrayTest {
     @Test
     public void arrayToSparseArray() {
@@ -14,5 +16,7 @@ public class SparseArrayTest {
         chessArr1[2][3] = 2;
         chessArr1[4][5] = 2;
         SparseArray.arrayToSparseArray(chessArr1);
+
+        log.info("{}","ww");
     }
 }
